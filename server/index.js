cee3290e6e0690ace384b4e2fdb7cdf8aa04ce91
LightFromAStone <github@lightfromastone.me>
2022-02-24 12:11:06 -0700

    added rollbar

--- server/index.js
+++ server/index.js
@@ -6,6 +6,20 @@ app.get('/', (req, res) => {
    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '3eedc999bfc6496795e524d12b628073',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+
+
 const port = process.env.PORT || 4545
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
