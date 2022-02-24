dedf7e537f446fea23a1741cece328843e9a4d05
LightFromAStone <github@lightfromastone.me>
2022-02-24 12:36:43 -0700

    added post endpoint

--- server/index.js
+++ server/index.js
@@ -2,10 +2,6 @@ const express = require('express');
 const path = require('path'); // comes from node
 const app = express();
 
-app.get('/', (req, res) => {
-   res.sendFile(path.join(__dirname, '../public/index.html'));
-});
-
 
 // include and initialize the rollbar library with your access token
 var Rollbar = require('rollbar')
@@ -17,6 +13,27 @@ var rollbar = new Rollbar({
 
 // record a generic message and send it to Rollbar
 rollbar.log('Hello world!')
+
+
+
+app.get('/', (req, res) => {
+   res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+
+let students = [];
+
+app.post('/api/student', (req, res)=>{
+   let {name} = req.body
+   name = name.trim()
+
+   students.push(name)
+
+   rollbar.log('student was added successfully', {author: 'Peter', type: 'manual entry'});
+   res.status(200).send(students)
+})
+
+app.use(rollbar.errorHandler());
 
 
 
